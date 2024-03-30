856b2beca18d44712393114325cf32effb8bd88f
JohnnyLove777 <47362296+JohnnyLove777@users.noreply.github.com>
2024-03-30 16:56:55 -0300

    path antigo localauth

--- sendMessage.js
+++ sendMessage.js
@@ -123,32 +123,38 @@ initializeWebhookDB();
     }
 });*/
   
-  //Kit com os comandos otimizados para nuvem Ubuntu Linux (créditos Pedrinho da Nasa Comunidade ZDG)
-  const client = new Client({
-    authStrategy: new LocalAuth({ clientId: sessao }),
-    puppeteer: {
-      headless: true,
-      //CAMINHO DO CHROME PARA WINDOWS (REMOVER O COMENTÁRIO ABAIXO)
-      //executablePath: 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe',
-      //===================================================================================
-      // CAMINHO DO CHROME PARA MAC (REMOVER O COMENTÁRIO ABAIXO)
-      //executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-      //===================================================================================
-      // CAMINHO DO CHROME PARA LINUX (REMOVER O COMENTÁRIO ABAIXO)
-       executablePath: '/usr/bin/google-chrome-stable',
-      //===================================================================================
-      args: [
-        '--no-sandbox', //Necessário para sistemas Linux
-        '--disable-setuid-sandbox',
-        '--disable-dev-shm-usage',
-        '--disable-accelerated-2d-canvas',
-        '--no-first-run',
-        '--no-zygote',
-        '--single-process', // <- Este não funciona no Windows, apague caso suba numa máquina Windows
-        '--disable-gpu'
-      ]
-    }
-  });
+const wwebVersion = '2.2412.50';
+
+//Kit com os comandos otimizados para nuvem Ubuntu Linux (créditos Pedrinho da Nasa Comunidade ZDG)
+const client = new Client({
+  authStrategy: new LocalAuth({ clientId: sessao }),
+  puppeteer: {
+    headless: true,
+    //CAMINHO DO CHROME PARA WINDOWS (REMOVER O COMENTÁRIO ABAIXO)
+    //executablePath: 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe',
+    //===================================================================================
+    // CAMINHO DO CHROME PARA MAC (REMOVER O COMENTÁRIO ABAIXO)
+    //executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+    //===================================================================================
+    // CAMINHO DO CHROME PARA LINUX (REMOVER O COMENTÁRIO ABAIXO)
+     executablePath: '/usr/bin/google-chrome-stable',
+    //===================================================================================
+    args: [
+      '--no-sandbox', //Necessário para sistemas Linux
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process', // <- Este não funciona no Windows, apague caso suba numa máquina Windows
+      '--disable-gpu'
+    ]
+  },
+  webVersionCache: {
+    type: 'remote',
+    remotePath: `https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/${wwebVersion}.html`,
+  }
+});
 
   async function sendMessageWithRetry(phoneNumber, messageToSend) {
     try {
